edc26898780817642609d6a509a1172de0b7b8b4
sSplendidSs <smyonlys@gmail.com>
2019-03-24 01:47:53 +0800

    hw1

--- wireless.py
+++ wireless.py
@@ -4,37 +4,62 @@ import matplotlib.pyplot as plt
 Pt=(10**(33/10))/1000
 Gt=10**(14/10)
 Gr=10**(14/10)
-print(Pt)
+#print(Pt)
 ht=50+1.5
 hr=1.5
 k=1.38*10**(-23)
 T=273+27
-B=10*10**6
+B=10*(10**6)
 N=k*T*B
+interference=0
+
 Pr=list()
 dist=list()
-'''
-for d in range(1,10000):
+#path loss
+for d in range(1,1000):
 	g=((ht*hr)**2)/(d**4)
-	P=g*Pt*Gr*Gr
+	P=g*Pt*Gt*Gr
 	Pr.append(10*math.log(P))
 	dist.append(d)
+
+plt.subplot(2,2,1)
 plt.plot(dist, Pr)
-plt.show()'''
 
-'''
-for d in range(1,10000):
+Pr=list()
+dist=list()
+#path loss's SINR
+for d in range(1,1000):
 	g=((ht*hr)**2)/(d**4)
-	P=10**(np.random.normal(0,6)/10)*g*Pt*Gr*Gr
-	Pr.append(10*math.log(P))
-	dist.append(d)'''
-
-for d in range(1,10000):
-	g=((ht*hr)**2)/(d**4)
-	P=10**(np.random.normal(0,6)/10)*g*Pt*Gr*Gr
-	SINR=P/N
+	P=g*Pt*Gt*Gr
+	SINR=P/(N+interference)
 	Pr.append(10*math.log(SINR))
 	dist.append(d)
 
+plt.subplot(2,2,3)
+plt.plot(dist, Pr)
+
+Pr=list()
+dist=list()
+#path loss and shadowing 
+for d in range(1,1000):
+	g=((ht*hr)**2)/(d**4)
+	P=(10**(np.random.normal(0,6)/10))*g*Pt*Gt*Gr
+	Pr.append(10*math.log(P))
+	dist.append(d)
+
+plt.subplot(2,2,2)
+plt.plot(dist, Pr)
+
+Pr=list()
+dist=list()
+#path loss and shadowing's SINR
+for d in range(1,1000):
+	g=((ht*hr)**2)/(d**4)
+	P=(10**(np.random.normal(0,6)/10))*g*Pt*Gt*Gr
+	SINR=P/(N+interference)
+	Pr.append(10*math.log(SINR))
+	dist.append(d)
+
+plt.subplot(2,2,4)
 plt.plot(dist, Pr)
 plt.show()
